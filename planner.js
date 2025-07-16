7a113e06589fd5acebe6806b338c9a2ea90a6d79
Daon Park <deeopark39@gmail.com>
2025-07-17 00:24:32 +0900

    test

--- planner.js
+++ planner.js
@@ -1,31 +1,46 @@
+// colors
 const COLOR_BG     = new Color("242424", 1)
 const COLOR_WHITE  = new Color("FFFFFF", 1)
-const COLOR_PURPLE = new Color("8C107A", 1)
-const COLOR_GREEN  = new Color("518E18", 1)
+const COLOR_GRAY   = new Color("808080", 1)
 const COLOR_ORANGE = new Color("FF671E", 1)
 
-const SIZE_TOTAL       = new Size(320, 160)
+// major stack sizes
+// const SIZE_TOTAL    = new Size(320, 160)
 const SIZE_STACK_D     = new Size(320,  40)
-const SIZE_STACK_E     = new Size(320,  10)
+const SIZE_STACK_H     = new Size(320,  10)
 const SIZE_STACK_L     = new Size(320,   2)
 const SIZE_STACK_C     = new Size(320, 110)
+const SIZE_STACK_E     = new Size(160, 110)
+const SIZE_STACK_R     = new Size(160, 110)
 
+// date stack sizes
 const SIZE_STACK_DB    = new Size( 40,  40)
 const SIZE_STACK_DAY   = new Size( 40,  10)
+const SIZE_STACK_DATE  = new Size( 40,  20)
 const SIZE_STACK_ALERT = new Size( 40,  10)
 const SIZE_STACK_BDAY  = new Size( 40,  15)
 const SIZE_STACK_BDATE = new Size( 40,  25)
 
-const SIZE_STACK_EB    = new Size( 10,  10)
+// habit stack sizes
+const SIZE_STACK_HB    = new Size( 10,  10)
 const SIZE_DOT         = 50
 const SIZE_ACTUAL_DOT  = new Size(  4,   4)
 
+// event stack sizes
+const SIZE_EVENT_PAD           = new Size(140,  5)
+const SIZE_EVENT_ALL_DAY_TITLE = new Size(140, 10)
+const SIZE_EVENT_TITLE         = new Size(140, 20)
+const SIZE_EVENT_TIME          = new Size(140, 10)
+const SIZE_EVENT_REMAINDER     = new Size(140, 10)
+
+// fonts
 const FONT_DAY         = Font.regularMonospacedSystemFont(8)
 const FONT_DATE        = Font.mediumSystemFont(20)
 const FONT_ALERT       = Font.mediumSystemFont(8)
-const FONT_BDAY        = Font.mediumMonospacedSystemFont(10)
-const FONT_BDATE       = Font.semiboldSystemFont(25)
+const FONT_BDAY        = Font.semiboldMonospacedSystemFont(11)
+const FONT_BDATE       = Font.semiboldSystemFont(30)
 
+// date formats
 const DF_DAY  = new DateFormatter()
 DF_DAY.dateFormat = "E"
 const DF_DATE = new DateFormatter()
@@ -58,6 +73,14 @@ function addWhiteText(stack, content, font)
   text.textColor = COLOR_WHITE
 }
 
+function addLine(stack, size, color)
+{
+  const stack_child = stack.addStack()
+
+  stack_child.size = size
+  stack_child.backgroundColor = color
+}
+
 function addStack(stack, size, color)
 {
   const stack_child = stack.addStack()
@@ -68,11 +91,66 @@ function addStack(stack, size, color)
   return stack_child
 }
 
-function drawExcercises(stack)
+function drawReminders(stack)
+{
+}
+
+function drawEvents(stack, events)
+{
+  const stack_p0   = buildStack(stack,           SIZE_EVENT_PAD, COLOR_WHITE)
+  const stack_adt0 = buildStack(stack, SIZE_EVENT_ALL_DAY_TITLE, COLOR_WHITE)
+  const stack_p1   = buildStack(stack,           SIZE_EVENT_PAD,    COLOR_BG)
+  const stack_adt1 = buildStack(stack, SIZE_EVENT_ALL_DAY_TITLE, COLOR_WHITE)
+  const stack_p2   = buildStack(stack,           SIZE_EVENT_PAD,    COLOR_BG)
+  const stack_t0   = buildStack(stack,         SIZE_EVENT_TITLE, COLOR_WHITE)
+  const stack_d0   = buildStack(stack,          SIZE_EVENT_TIME, COLOR_WHITE)
+  const stack_p3   = buildStack(stack,           SIZE_EVENT_PAD,    COLOR_BG)
+  const stack_t1   = buildStack(stack,         SIZE_EVENT_TITLE, COLOR_WHITE)
+  const stack_d1   = buildStack(stack,          SIZE_EVENT_TIME, COLOR_WHITE)
+  const stack_p4   = buildStack(stack,           SIZE_EVENT_PAD,    COLOR_BG)
+  const stack_r    = buildStack(stack,     SIZE_EVENT_REMAINDER, COLOR_WHITE)
+
+  // find today's events
+  let all_day_events_today = []
+  let events_today = []
+  for (const e of events)
+  {
+    const text_event_start = DF_TIME.string(e.startDate)
+    const text_event_end = DF_TIME.string(e.endDate)
+    const compare_start = text_event_start.localeCompare("00:00")
+    const compare_end = text_event_end.localeCompare("23:59")
+
+    if (compare_start == 0 && compare_end == 0)
+      addEvent(all_day_events_today, e)
+    else if (e.startDate.getTime() > today.getTime())
+      addEvent(events_today, e)
+  }
+
+//  // check for first all day event
+//  if (all_day_events_today.length > 0)
+//    buildAllDayEventStack(all_day_events_today[0], stack_adt0)
+//  // check for second all day event
+//  if (all_day_events_today.length > 1)
+//    buildAllDayEventStack(all_day_events_today[1], stack_adt1)
+//  // check for today's first event
+//  if (events_today.length > 0)
+//    buildEventStack(events_today[0], stack_t0, stack_d0)
+//  // check for today's second event
+//  if (events_today.length > 1)
+//    buildEventStack(events_today[1], stack_t1, stack_d1)
+//  // check if there are three events today
+//  if (events_today.length == 3)
+//    buildRemainderStack(stack_r, 1, "event")
+//  // check if there are four or more events today
+//  else if (events_today.length > 3)
+//    buildRemainderStack(stack_r, events_today.length - 2, "events")
+}
+
+function drawHabits(stack)
 {
   for (let i = 0; i < 31; ++i)
   {
-    const stack_block = addStack(stack, SIZE_STACK_EB, COLOR_BG)
+    const stack_block = addStack(stack, SIZE_STACK_HB, COLOR_BG)
 
     drawDot(stack_block, /*dot_resolution*/ 50, SIZE_ACTUAL_DOT, COLOR_WHITE)
   }
@@ -82,9 +160,7 @@ function drawDates(stack)
 {
   for (let i = 0; i < 7; ++i)
   {
-    // create date
     const date = new Date(new Date().getTime() + i * 24 * 60 * 60 * 1000)
-    // create block stack
     const stack_block = addStack(stack, SIZE_STACK_DB, COLOR_BG)
 
     // create day, date, and alert stack
@@ -119,19 +195,19 @@ async function buildMediumWidget()
   const today = new Date()
 
   stack.layoutVertically()
-  // create dates, excercises, line, and contents stack
-  const stack_d = addStack(stack, SIZE_STACK_D, COLOR_BG)
-  const stack_e = addStack(stack, SIZE_STACK_E, COLOR_BG)
-  const stack_l = stack.addStack()
-  const stack_c = stack.addStack()
-
-  stack_l.size = SIZE_STACK_L
-  stack_l.backgroundColor = COLOR_WHITE
-  stack_c.size = SIZE_STACK_C
-  stack_c.backgroundColor = COLOR_ORANGE
+  // create date, habit, line, and content stack
+  const stack_d = addStack(  stack, SIZE_STACK_D, COLOR_BG)
+  const stack_h = addStack(  stack, SIZE_STACK_H, COLOR_BG)
+  addLine(stack, SIZE_STACK_L, COLOR_WHITE)
+  const stack_c = addStack(  stack, SIZE_STACK_C, COLOR_BG)
+  // create event and reminder stack
+  const stack_e = addStack(stack_c, SIZE_STACK_E:, COLOR_ORANGE)
+  const stack_r = addStack(stack_c, SIZE_STACK_R, COLOR_GRAY)
 
   drawDates(stack_d)
-  drawExcercises(stack_e)
+  drawHabits(stack_h)
+  drawEvents(stack_e, events)
+  drawReminders(stack_r)
 
   return widget
 }
